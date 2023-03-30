7474cd5da5d9d58916cedf25af7ae89c1555df9c
JanaKoduje <jjanule@gmail.com>
2023-03-30 21:59:39 +0200

    mixer in react

---
+++ src/Components/ColorSlider.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ColorSlider = ({ baseColor, colorName, onValueChange }) => {
+
+    return (
+        <>
+            <label htmlFor={baseColor}>{colorName}</label>
+            <input
+                type="range"
+                className={`slider slider--${baseColor}`}
+                id={`${baseColor}Slider`}
+                min="0"
+                max="255"
+                defaultValue="0"
+                onChange={(e) => onValueChange(e.target.value)}
+            />
+        </>)
+}
+
+export default ColorSlider;
